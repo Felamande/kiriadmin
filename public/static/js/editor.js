72b4ae5b15e9eafcd7681cc3eef0a564ae4abf5d
Felamande-Arch <elweinfancon@gmail.com>
2016-06-15 17:12:12 +0800

    editor.js

--- public/static/js/editor.js
+++ public/static/js/editor.js
@@ -1,6 +1,33 @@
 window.onload = function() {
     Vue.use(VueResource)
-    new Vue({
-      
+
+    window.vm = new Vue({
+        el:".container",
+        data:{
+            mdSrc:"",
+            mdCvt:"",
+            mdPrv:document.getElementById("md-preview")
+        },
+        components:{
+            group:vuxGroup,
+            cell:vuxCell,
+            "x-textarea":vuxXTextarea,
+            flexbox:vuxFlexbox,
+            "flexbox-item":vuxFlexboxItem,
+            "x-button":vuxXButton
+        },
+        methods:{
+            postPreview:function() {
+                this.$http.post("/editor/preview",{"content":this.mdSrc,"_xsrf":document.getElementsByName('_xsrf')[0].value},{headers:{"Content-Type":"application/json"}}).then(
+                    function(response){
+                        document.getElementById("md-preview").innerHTML = response.data
+                    },function(error){}
+                )
+            }
+        }
     })
+}
+
+window.onbeforeunload = function(){
+    return (function(){return "hello"})()
 }
