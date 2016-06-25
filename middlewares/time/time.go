7550c784c23b4c70084782f28507f29dfd7378b0
Felamande-Arch <elweinfancon@gmail.com>
2016-06-25 18:05:58 +0800

    remove ua log

--- middlewares/time/time.go
+++ middlewares/time/time.go
@@ -1,27 +1,22 @@
 package time
 
 import (
-	"regexp"
 	"time"
 
 	"github.com/lunny/tango"
 )
-
-var uaRegexp = regexp.MustCompile(`(Android|Windows|iPhone|iPad|MicroMessenger|MSIE|Chrome|QQ)`)
 
 type TimeHandler struct {
 }
 
 func (h *TimeHandler) Handle(ctx *tango.Context) {
 	t1 := time.Now()
-	// ctx.Logger.Info(ctx.Header())
 	ctx.Next()
-	ctx.Logger.Infof("Completed %v %v %v in %v for %v %v",
+	ctx.Logger.Infof("Completed %v %v %v in %v for %v",
 		ctx.Status(),
 		ctx.Req().Method,
 		ctx.Req().URL.Path,
 		time.Since(t1),
 		ctx.Req().RemoteAddr,
-		uaRegexp.FindAllString(ctx.Req().Header.Get("User-Agent"), -1),
 	)
 }
