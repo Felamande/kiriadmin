9ffea0fad743601597a8a904eca41d37d2864739
Felamande-Arch <elweinfancon@gmail.com>
2016-06-26 12:52:40 +0800

    fix

--- public/js/editor.js
+++ public/js/editor.js
@@ -20,9 +20,16 @@ window.onload = function() {
             postPreview:function() {
                 this.$http.post("/editor/preview",{"content":this.mdSrc,"_xsrf":document.getElementsByName('_xsrf')[0].value},{headers:{"Content-Type":"application/json"}}).then(
                     function(response){
+                        if(typeof response.data == 'object'){
+                            this.handleError(response.data.err)
+                            return
+                        }
                         document.getElementById("md-preview").innerHTML = response.data
                     },function(error){}
                 )
+            },
+            handleError:function(err){
+                console.log(err)
             }
         }
     })
